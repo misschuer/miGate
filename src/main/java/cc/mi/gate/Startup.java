97531af823c6d6adc96a7fdae5f0b8f66c36a9dd
xcsj <xcsj@192.168.0.108>
2018-12-20 10:46:14 +0800

    修改逻辑

--- src/main/java/cc/mi/gate/Startup.java
+++ src/main/java/cc/mi/gate/Startup.java
@@ -1,5 +1,8 @@
 package cc.mi.gate;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import cc.mi.core.constance.IdentityConst;
 import cc.mi.core.log.CustomLogger;
 import cc.mi.core.net.ServerCore;
@@ -12,43 +15,60 @@ public class Startup {
 	
 	private static void start() throws NumberFormatException, Exception {
 		ServerConfig.loadConfig();
-		new Thread(new Runnable() {
-			@Override
-			public void run() {
-				while (true) {
-					try {
-						ServerCore.INSTANCE.run(ServerConfig.getOuterPort(), new GateHandler(), IdentityConst.SERVER_TYPE_GATE);
-					} catch (Exception e) {
-					} finally {
-						logger.devLog("监听客户端端口发生错误,系统将在1秒钟后重新执行");
-						try {
-							Thread.sleep(1000);
-						} catch (Exception e) {
-							e.printStackTrace();
-						}
-					}
-				}
-			}
-		}, "gate-for-clients").start();
+
+		bindClient();
 		
-		new Thread(new Runnable() {
-			@Override
-			public void run() {
-				while (true) {
-					try {
-						ServerCore.INSTANCE.run(ServerConfig.getInnerPort(), new GateLocalHandler(), IdentityConst.SERVER_TYPE_GATE);
-					} catch (Exception e) {
-					} finally {
-						logger.devLog("监听内部服务器端口发生错误,系统将在1秒钟后重新执行");
+		bindInnerServer();
+	}
+	
+	private static void bindClient() {
+		ExecutorService executor = Executors.newSingleThreadExecutor();
+		executor.execute(
+			new Runnable() {
+				@Override
+				public void run() {
+					while (true) {
 						try {
-							Thread.sleep(1000);
+							ServerCore.INSTANCE.run(ServerConfig.getOuterPort(), new GateHandler(), IdentityConst.SERVER_TYPE_GATE);
 						} catch (Exception e) {
-							e.printStackTrace();
+							logger.errorLog(e.getMessage());
+						} finally {
+							logger.errorLog("监听客户端端口发生错误,系统将在1秒钟后重新执行");
+							try {
+								Thread.sleep(1000);
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
 						}
 					}
 				}
 			}
-		}, "gate-for-inner").start();
+		);
+	}
+	
+	private static void bindInnerServer() {
+		ExecutorService executor = Executors.newSingleThreadExecutor();
+		executor.execute(
+			new Runnable() {
+				@Override
+				public void run() {
+					while (true) {
+						try {
+							ServerCore.INSTANCE.run(ServerConfig.getInnerPort(), new GateLocalHandler(), IdentityConst.SERVER_TYPE_GATE);
+						} catch (Exception e) {
+							logger.errorLog(e.getMessage());
+						} finally {
+							logger.errorLog("监听内部服务器端口发生错误,系统将在1秒钟后重新执行");
+							try {
+								Thread.sleep(1000);
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
+						}
+					}
+				}
+			}
+		);
 	}
 
 	public static void main(String[] args) throws NumberFormatException, Exception {
